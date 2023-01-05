bf6f1d7e998784e46d6a9544da273590258a8930
Fr4nc1e <75559690+Fr4nc1e@users.noreply.github.com>
2023-01-05 10:45:35 +0800

    Make the code readable

--- app/src/main/java/com/code/block/presentation/components/StandardScaffold.kt
+++ app/src/main/java/com/code/block/presentation/components/StandardScaffold.kt
@@ -1,23 +1,14 @@
 package com.code.block.presentation.components
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.* // ktlint-disable no-wildcard-imports
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.code.block.R
-import com.code.block.domain.model.BottomNavItem
-import com.code.block.presentation.NavGraphs
-import com.ramcosta.composedestinations.navigation.navigate
-import com.ramcosta.composedestinations.navigation.popBackStack
-import com.ramcosta.composedestinations.navigation.popUpTo
-import com.ramcosta.composedestinations.utils.isRouteOnBackStack
 
 @Composable
 fun StandardScaffold(
@@ -29,43 +20,10 @@ fun StandardScaffold(
 ) {
     Scaffold(
         bottomBar = {
-            if (showBottomBar) {
-                BottomAppBar(
-                    modifier = Modifier.fillMaxWidth(),
-                    backgroundColor = MaterialTheme.colors.surface,
-                    cutoutShape = CircleShape,
-                    elevation = 5.dp
-                ) {
-                    BottomNavigation {
-                        BottomNavItem.values().forEach {
-                            val isCurrentDestinationOnBackStack = navController
-                                .isRouteOnBackStack(route = it.route)
-                            StandardBottomNavItem(
-                                icon = it.icon,
-                                contentDescription = stringResource(id = it.contentDescription),
-                                selected = isCurrentDestinationOnBackStack,
-                                alertCount = it.alertCount,
-                                enabled = it.icon != null,
-                                onClick = {
-                                    if (isCurrentDestinationOnBackStack) {
-                                        navController.popBackStack(it.route, false)
-                                        return@StandardBottomNavItem
-                                    }
-
-                                    navController.navigate(it.route) {
-                                        popUpTo(NavGraphs.root) {
-                                            saveState = true
-                                        }
-
-                                        launchSingleTop = true
-                                        restoreState = true
-                                    }
-                                }
-                            )
-                        }
-                    }
-                }
-            }
+            StandardBottomBar(
+                navController = navController,
+                showBottomBar = showBottomBar
+            )
         },
         floatingActionButton = {
             if (showBottomBar) {
