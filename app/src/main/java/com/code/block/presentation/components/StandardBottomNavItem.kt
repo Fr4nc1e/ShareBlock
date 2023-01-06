4710d2d8842f4fb9779ff31f57903fd2f384aa7c
Fr4nc1e <75559690+Fr4nc1e@users.noreply.github.com>
2023-01-06 14:19:47 +0800

    Change alert ui to BadgeBox

--- app/src/main/java/com/code/block/presentation/components/StandardBottomNavItem.kt
+++ app/src/main/java/com/code/block/presentation/components/StandardBottomNavItem.kt
@@ -2,17 +2,11 @@ package com.code.block.presentation.components
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.* // ktlint-disable no-wildcard-imports
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -78,34 +72,44 @@ fun RowScope.StandardBottomNavItem(
                     }
             ) {
                 if (icon != null) {
-                    Icon(
-                        imageVector = icon,
-                        contentDescription = contentDescription,
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                    )
-                }
+                    if (alertCount != null) {
+                        BadgedBox(
+                            modifier = Modifier.align(Alignment.Center),
+                            badge = {
+                                Badge(
+                                    backgroundColor = MaterialTheme.colors.primary
+                                ) {
+                                    val alertText = if (alertCount > 99) {
+                                        "99"
+                                    } else {
+                                        alertCount.toString()
+                                    }
 
-                if (alertCount != null) {
-                    val alertText = if (alertCount > 99) {
-                        "99"
+                                    Text(
+                                        text = alertText,
+                                        fontSize = 10.sp,
+                                        color = MaterialTheme.colors.onPrimary,
+                                        fontWeight = FontWeight.Bold,
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = icon,
+                                contentDescription = contentDescription,
+                                modifier = Modifier
+                                    .align(Alignment.Center)
+                            )
+                        }
                     } else {
-                        alertCount.toString()
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = contentDescription,
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                        )
                     }
-
-                    Text(
-                        text = alertText,
-                        color = MaterialTheme.colors.onPrimary,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        fontSize = 10.sp,
-                        modifier = Modifier
-                            .align(Alignment.TopCenter)
-                            .offset(10.dp)
-                            .size(15.dp)
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colors.primary)
-                    )
                 }
             }
         }
